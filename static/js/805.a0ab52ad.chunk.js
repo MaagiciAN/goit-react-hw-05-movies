7909828009685b268ca9137229980a1cd7d0f750
MaagiciAN <134884643+MaagiciAN@users.noreply.github.com>
2024-01-25 20:38:11 +0000

    Deploying to gh-pages from @ MaagiciAN/goit-react-hw-05-movies@32a116962a8b73d2c1ff2795908149b7d63ada67 🚀

---
+++ static/js/805.a0ab52ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,n,t){t.r(n);var r=t(87),s=t(184);n.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("p",{children:"Page is not found"}),(0,s.jsx)(r.rU,{to:"/",children:"Go home "})]})}}}]);
+//# sourceMappingURL=805.a0ab52ad.chunk.js.map
